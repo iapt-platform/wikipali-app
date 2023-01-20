01a905f6d2a7bf5c09430d172cfcc916ce675e9e
ariyamaggika <117618324+ariyamaggika@users.noreply.github.com>
2023-01-20 09:14:44 +0800

    底部菜单toggle

--- Assets/Scripts/View/UI/MainView.cs
+++ Assets/Scripts/View/UI/MainView.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainView : MonoBehaviour
 {
+    public Toggle dicToggle;
+    public Toggle articleToggle;
+    public Toggle userToggle;
+    public Toggle settingToggle;
+    public DicView dicView;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
