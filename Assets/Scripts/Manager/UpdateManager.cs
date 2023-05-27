647ef1816e6c5a6dc9e0a0970a47592228bb5d98
ariyamaggika <117618324+ariyamaggika@users.noreply.github.com>
2023-05-27 10:08:09 +0800

    apk更新

---
+++ Assets/Scripts/Manager/UpdateManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+//°æ±¾¸üÐÂ
+public class UpdateManager : MonoBehaviour
+{
+    
+
+
+}
